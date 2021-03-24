d1a296bf0f4dfa0547ae4086b32ac6466cf6c215
Angelika <kozlovaangelika09@gmail.com>
2021-03-24 10:29:01 +0300

    feat: add slider

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,8 +1,8 @@
 const burger = document.querySelector('.burger');
 
-
 function init() {
     burger.addEventListener('click', toggleMenu);
+    sliderInit();
 }
 
 function toggleMenu() {
@@ -12,4 +12,46 @@ function toggleMenu() {
     document.querySelector('body').classList.toggle('lock')
 }
 
+
+
+//слайдер
+const sliderHTML = document.querySelector('.slider');
+const slidesHTML = document.querySelectorAll('.slider .slide');
+const leftArrowHTML = document.querySelector('.wr-slider .slider-buttons .arrow-left');
+const rightArrowHTML = document.querySelector('.wr-slider .slider-buttons .arrow-right');
+let count = 0;
+let width;
+
+function sliderInit() {
+    width = sliderHTML.offsetWidth;
+}
+sliderHTML.style.width = width * slidesHTML.length + '%';
+slidesHTML.forEach(e => {
+    e.style.width = width + '%'
+})
+window.addEventListener('resize', sliderInit);
+
+function rollSliderLeft() {
+    rightArrowHTML.disabled = false;
+    if (count <= 1) {
+        leftArrowHTML.disabled = true;
+    } else {
+        leftArrowHTML.disabled = false;
+    }
+    count--;
+    sliderHTML.style.transform = `translate(-${count*100}%)`
+}
+
+function rollSliderRight() {
+    if (count >= slidesHTML.length - 2) {
+        rightArrowHTML.disabled = true;
+    } else {
+        rightArrowHTML.disabled = false;
+    }
+    leftArrowHTML.disabled = false;
+    count++;
+    sliderHTML.style.transform = `translate(-${count*100}%)`
+}
+leftArrowHTML.addEventListener('click', rollSliderLeft)
+rightArrowHTML.addEventListener('click', rollSliderRight)
 init();
